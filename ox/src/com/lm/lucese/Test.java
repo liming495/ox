cbab4f7d836991ded6146450e1d35f18ffaaa487
liming <wop129@tom.com>
2015-10-19 17:34:25 +0800

    添加测试

--- ox/src/com/lm/lucese/Test.java
+++ ox/src/com/lm/lucese/Test.java
@@ -2,4 +2,12 @@ package com.lm.lucese;
 
 public class Test {
 
+	public static void main(String[] args) {
+		System.out.println(System.getProperty("os.name"));
+	}
+
+	public void test() {
+
+	}
+
 }
